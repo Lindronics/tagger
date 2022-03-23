4a11a7b856cdaf78f78625a7d2b8f98829b8503d
Niklas <niklas@lindorfer.com>
2022-03-23 22:25:18 +0000

    Refactored

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,74 @@
 use std::{collections::HashSet, iter::FromIterator};
 
-use git2::{string_array::StringArray, Error, Oid, Repository, Tag};
+use ansi_term::Colour::Yellow;
+use git2::{Error, Oid, Repository, Tag};
 use semver::Version;
 use substring::Substring;
+
+struct TagVersion<'a> {
+    tag: Tag<'a>,
+    version: Version,
+}
 
 fn main() {
     let path = "/Users/lindronics/workspace/tests/tag_test";
     let repo = Repository::open(path).unwrap();
 
-    let revwalk = get_branch_commits(&repo).unwrap();
-    let current_branch_commits = HashSet::<Oid>::from_iter(revwalk.filter_map(Result::ok));
-    // set.iter().for_each(|x| println!("-> {:?}", x));
-
-    let tag_names = repo.tag_names(None).unwrap();
-    let latest_release = get_versions(&repo, &tag_names)
-        .filter(|v| v.pre == Default::default())
-        .max()
-        .unwrap_or_else(|| Version::new(0, 0, 0));
-
-    let pre_tags = get_versions(&repo, &tag_names)
-        .filter(|v| v.pre != Default::default())
-        .filter(|v| v > &latest_release);
-
-    println!("Latest release: {:?}", latest_release);
-    pre_tags.for_each(|t| println!("{}", t.to_string()))
+    get_latest_tags(&repo)
 }
 
-fn get_versions<'a, 'b>(
-    repo: &'a Repository,
-    tag_names: &'a StringArray,
-) -> impl Iterator<Item = Version> + 'a {
-    // let tag_names = repo.tag_names(None).unwrap();
-    let x = tag_names
+fn get_latest_tags(repo: &Repository) {
+    let revwalk = get_branch_commits(&repo).unwrap();
+    let current_branch_commits = HashSet::<Oid>::from_iter(revwalk.filter_map(Result::ok));
+
+    let tag_names = repo.tag_names(None).unwrap();
+
+    let versions = tag_names
         .iter()
-        .filter_map(move |x| x.and_then(|y| get_tag(&repo, y).ok()))
-        .map(|tag| parse_version(&tag).unwrap());
-    return x;
+        .filter_map(|name| name)
+        .filter_map(|name| get_tag_version(&repo, name))
+        .collect::<Vec<_>>();
+
+    let latest_release = &versions
+        .iter()
+        .filter(|v| v.version.pre.is_empty())
+        .map(|v| v.version.clone())
+        .max()
+        .unwrap_or(Version::new(0, 0, 0));
+
+    let pre_tags = &versions
+        .iter()
+        .filter(|v| !v.version.pre.is_empty())
+        .filter(|v| v.version.gt(latest_release))
+        .collect::<Vec<_>>();
+
+    let current_branch_pre_tag = pre_tags
+        .iter()
+        .filter(|v| current_branch_commits.contains(&v.tag.target().unwrap().id()))
+        .max_by(|x, y| x.version.cmp(&y.version));
+
+    println!("\nLatest tags:");
+    println!(
+        " {} - (main)",
+        Yellow.paint(format!("v{: <10}", &latest_release))
+    );
+    println!(
+        " {} - (current branch)",
+        Yellow.paint(format!(
+            "v{: <10}",
+            &current_branch_pre_tag.unwrap().version
+        ))
+    );
+    println!("\nOther branches:");
+    pre_tags
+        .iter()
+        .for_each(|t| println!(" {}", Yellow.paint(format!("v{}", &t.version.to_string()))));
+}
+
+fn get_tag_version<'a>(repo: &'a Repository, tag_name: &str) -> Option<TagVersion<'a>> {
+    let tag = get_tag(&repo, tag_name).ok()?;
+    let version = parse_version(&tag)?;
+    Some(TagVersion { tag, version })
 }
 
 // Get tag object from name
